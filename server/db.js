dc077a3c82d5141535b6bce436a3e1931a2e05a2
31pablo05 <Pabloproboste64@gmail.com>
2023-12-07 19:01:52 -0500

    commit

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:MTQ3MS1wYWJsb3By@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MjAyOTYtcGFibG9w@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
