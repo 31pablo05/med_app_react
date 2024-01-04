2a8d3df312b74ba0a0f695c80c97db9690d5feae
31pablo05 <Pabloproboste64@gmail.com>
2024-01-03 20:08:31 -0500

    comit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-export const API_URL = window.location.hostname === "localhost" ? "https://pabloprobost-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "add your theia server side url";
-console.log(
-    "API_URL :",
-    API_URL
-);
+export const API_URL = window.location.hostname === "localhost" ?
+  "https://pabloprobost-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" :
+  "";
